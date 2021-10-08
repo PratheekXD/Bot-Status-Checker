8d48ae1a57ebc63af4beccff3f211cf817a29cdf
Pratheek <89387738+PratheekXD@users.noreply.github.com>
2021-10-08 05:52:13 +0530

    Done

---
+++ README.md
@@ -0,0 +1,27 @@
+<h1 align= center> BOTS-STATUS-CHECKER</h1>
+<h3 align = center>Easy & powerful bot to check if your all Telegram bots are working or not. This bot status bot updates every 45 minutes & runs for 24x7 hours.</h3>
+<p align="center">
+<a href="https://python.org"><img src="http://forthebadge.com/images/badges/made-with-python.svg" alt="made-with-python"></a>
+
+## Config Vars
+1. `API_ID` : Telegram API_ID, get it from my.telegram.org/apps
+2. `API_HASH` : Telegram API_ID, get it from my.telegram.org/apps
+3. `SESSION_NAME` : A valid Pyrogram session name
+4. `BOT_LIST` : Your bot username list without '@' (Example: pratheek1bot pratheek2bot pratheek3bot
+5. `CHANNEL_OR_GROUP_ID` : Your channel's or group's Telegram id (Example: -1001246808642)
+6. `MESSAGE_ID` : Telegram id of message from your channel or group (Example: 10)
+7. `BOT_ADMIN_IDS` : Bot admin(s) id(s) (Example: 1357907531 2468097531 3579864213) - Not Required!
+8. `TIME_ZONE`: Your time zone (Example: Asia/Kolkata)
+
+<p align="center">
+<img src="https://telegra.ph/file/dd7e94f283ae2ca8aa613.jpg">
+<p>
+ 
+## Deployment Methods
+
+### Heroku
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/PratheekXD/BOT-ONLINE-CHECKER)
+ 
+### REPL
+[![GenerateString](https://img.shields.io/badge/repl.it-generateString-yellowgreen)](https://replit.com/@dashezup/generate-pyrogram-session-string)
---
+++ botstatus_pratheek.py
@@ -0,0 +1,55 @@
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+import asyncio
+import datetime
+import pytz
+import os
+
+app = Client(
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"],
+    session_name = os.environ["SESSION_NAME"]
+)
+TIME_ZONE = os.environ["TIME_ZONE"]
+BOT_LIST = [i.strip() for i in os.environ.get("BOT_LIST").split(' ')]
+CHANNEL_OR_GROUP_ID = int(os.environ["CHANNEL_OR_GROUP_ID"])
+MESSAGE_ID = int(os.environ["MESSAGE_ID"])
+BOT_ADMIN_IDS = [int(i.strip()) for i in os.environ.get("BOT_ADMIN_IDS").split(' ')]
+
+async def main_pratheek():
+    async with app:
+            while True:
+                print("Checking...")
+                GET_CHANNEL_OR_GROUP = await app.get_chat(int(CHANNEL_OR_GROUP_ID))
+                CHANNEL_OR_GROUP_NAME = GET_CHANNEL_OR_GROUP.title
+                CHANNEL_OR_GROUP_TYPE = GET_CHANNEL_OR_GROUP.type
+                xxx_pratheek = f"📊 𝙇𝙄𝙑𝙀 𝘽𝙊𝙏 𝙎𝙏𝘼𝙏𝙐𝙎\n\n**💬 {CHANNEL_OR_GROUP_TYPE}**: {CHANNEL_OR_GROUP_NAME}"
+                for bot in BOT_LIST:
+                    try:
+                        yyy_pratheek = await app.send_message(bot, "/start")
+                        aaa = yyy_pratheek.message_id
+                        await asyncio.sleep(10)
+                        zzz_pratheek = await app.get_history(bot, limit = 1)
+                        for ccc in zzz_pratheek:
+                            bbb = ccc.message_id
+                        if aaa == bbb:
+                            xxx_pratheek += f"\n\n🤖 **BOT**: @{bot}\n🔴 **STATUS**: down ❌"
+                            for bot_admin_id in BOT_ADMIN_IDS:
+                                try:
+                                    await app.send_message(int(bot_admin_id), f"🚨 **Beep! Beep!! @{bot} is down** ❌")
+                                except Exception:
+                                    pass
+                            await app.read_history(bot)
+                        else:
+                            xxx_pratheek += f"\n\n🤖 **BOT**: @{bot}\n🟢 **STATUS**: alive ✅"
+                            await app.read_history(bot)
+                    except FloodWait as e:
+                        await asyncio.sleep(e.x)            
+                time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
+                last_update = time.strftime(f"%d %b %Y at %I:%M %p")
+                xxx_pratheek += f"\n\n✔️ Last checked on: {last_update} ({TIME_ZONE})\n\n<i>♻️ Updates every 45min</i> - Made By [Dᴇᴠᴏʟᴏᴘᴇʀ🥀](https://t.me/pratheek06) 💙"
+                await app.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID, xxx_pratheek)
+                print(f"Last checked on: {last_update}")                
+                await asyncio.sleep(2700)
+                        
+app.run(main_pratheek())
